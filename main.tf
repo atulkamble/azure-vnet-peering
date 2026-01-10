23018f544670b929a1516ba6fcc07a90a9f8601c
Atul Kamble <atul_kamble@hotmail.com>
2026-01-10 09:49:37 +0530

    Add .gitignore and .terraformignore, fix resource group references in main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet_a" {
   name                = "vnet-a"
   address_space       = var.vnet_a_address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet_a" {
   name                 = "subnet-a"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet_a.name
   address_prefixes     = var.subnet_a_prefix
 }
@@ -22,13 +22,13 @@ resource "azurerm_subnet" "subnet_a" {
 resource "azurerm_virtual_network" "vnet_b" {
   name                = "vnet-b"
   address_space       = var.vnet_b_address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet_b" {
   name                 = "subnet-b"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet_b.name
   address_prefixes     = var.subnet_b_prefix
 }
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "subnet_b" {
 # VNet Peering A -> B
 resource "azurerm_virtual_network_peering" "peer_a_to_b" {
   name                      = "peer-a-to-b"
-  resource_group_name       = var.resource_group_name
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet_a.name
   remote_virtual_network_id = azurerm_virtual_network.vnet_b.id
   allow_virtual_network_access = true
@@ -45,7 +45,7 @@ resource "azurerm_virtual_network_peering" "peer_a_to_b" {
 # VNet Peering B -> A
 resource "azurerm_virtual_network_peering" "peer_b_to_a" {
   name                      = "peer-b-to-a"
-  resource_group_name       = var.resource_group_name
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet_b.name
   remote_virtual_network_id = azurerm_virtual_network.vnet_a.id
   allow_virtual_network_access = true
